ec67889a53fa9ba98d004528c92925f90a865361
Asashi-Git <samueldecarnelle@gmail.com>
2025-11-12 12:57:06 +0100

    Starting the main configuration script for the client

--- maintenance-Linux/HashRelay/hashrelay-installer.sh
+++ maintenance-Linux/HashRelay/hashrelay-installer.sh
@@ -155,7 +155,7 @@ if [[ "$CLIENT_AGENT" == true ]]; then
     sudo mv "$LOCATION_PATH/Dependencies/ufw-configuration-manager" "/usr/local/bin/HashRelay"
     printf 'CLIENT_AGENT=true\n' | sudo tee -a "/usr/local/bin/HashRelay/agent.conf" >/dev/null
     printf 'SERVER_AGENT=false\n' | sudo tee -a "/usr/local/bin/HashRelay/agent.conf" >/dev/null
-    printf '# Contain all the backup files paths\n/usr/local/bin/HashRelay' | sudo tee -a "/usr/local/bin/HashRelay/backups-manager/backups.conf" >/dev/null
+    printf '# Contain all the backup files paths\nHashRelay=/usr/local/bin/HashRelay' | sudo tee -a "/usr/local/bin/HashRelay/backups-manager/backups.conf" >/dev/null
     printf '# Contain all the hash for each backups file\n' | sudo tee -a "/usr/local/bin/HashRelay/hash-printer/hash.conf" >/dev/null
     sudo rm -rf "$LOCATION_PATH/Dependencies"
   fi
