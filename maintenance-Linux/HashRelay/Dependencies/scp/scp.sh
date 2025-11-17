301b6ddc8aa37ce7076874cd83fd38b0df2c5ef0
Asashi-Git <samueldecarnelle@gmail.com>
2025-11-17 16:16:10 +0100

    V1 of the uninstaller script

---
+++ maintenance-Linux/HashRelay/Dependencies/scp/scp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script is used to export to the server the modified backups.
+#
+# It can import backup from the server too either by dowloading all actual backup that
+# are actually onto the server or X last number of backups of the same file.
+#
+# Author: Decarnelle Samuel
---
+++ maintenance-Linux/HashRelay/Dependencies/sender/sender.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# This script is used by HashRelay client to retreave the hash file of
+# the backups of the actual user that are actually onto the server
